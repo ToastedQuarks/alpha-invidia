9d97ad688dda3848fac8952915faa251c68aa91d
ToastedQuarks <toastedquarks@gmail.com>
2017-04-22 19:19:00 -0400

    added window interval and log functions

--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function swapToMain() {
   document.getElementById("maintab").style.display="block";
   document.getElementById("techtree").style.display="none";
@@ -15,3 +17,22 @@ function swapToWar() {
   document.getElementById("techtree").style.display="none";
   document.getElementById("wartab").style.display="block";
 }
+
+function createLogMessage(message) {
+  var newLi = document.createElement("li");
+  var newText = document.createTextNode(message);
+  newLi.appendChild(newText);
+  var logList = document.getElementById("gameLog");
+  var msgCount = logList.getElementsByTagName("li").length;
+  if (msgCount > 100)
+    {
+      logList.removeChild(logList.lastChild);
+      logList.insertBefore(newLi, logList.firstChild);
+    }
+  else {
+    logList.insertBefore(newLi, logList.firstChild);
+  }
+}
+
+window.setInterval(function () {
+}, 1000);
