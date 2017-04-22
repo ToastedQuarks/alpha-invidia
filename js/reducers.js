031a33118296ab0327a75bbff904ebae0d8770be
Colin P. Hill <colin.patrick.hill@gmail.com>
2017-04-22 17:00:05 -0400

    Added a bunch of JS. We have state!

---
+++ js/reducers.js
@@ -0,0 +1,36 @@
+var initialState = {
+  time: 0,
+  planets: {
+	  "Alpha Invidia": {
+      name: "Alpha Invidia",
+	    food: 100,
+	    population: ["Romulus", "Remus"]
+	  }
+  },
+  people: {
+    "Romulus": {
+      name: "Romulus",
+      born: 0
+    },
+    "Remus": {
+      name: "Remus",
+      born: 0
+    }
+  }
+}
+
+function reduceGlobal(state, action) {
+  if (state === undefined) {
+    state = initialState
+  }
+  switch (action.type) {
+    case act.clockTick.type:
+      return Object.assign({}, state, {
+        time: state.time+1
+        //planets: state.planets.map((p) => reducePlanet(p, action))
+      })
+    default:
+      return state
+  }
+}
+
